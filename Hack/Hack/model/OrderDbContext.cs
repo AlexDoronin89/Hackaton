b93eb32f683df8d3bd2040277a7d299288b59e24
adoronin158@gmail.com <adoronin158@gmail.com>
2022-04-19 12:36:21 +0300

    123а12а3фац

---
+++ Hack/Hack/model/OrderDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hack.model
+{
+    internal class OrderDbContext : DbContext
+    {
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Supplier> Suppliers { get; set; }
+        public DbSet<Purchaser> Purchasers { get; set; }
+
+        public OrderDbContext()
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySql("Server=localhost;User=root;Password=1234;Database=zchg", new MySqlServerVersion(new Version()));
+        }
+
+    }
+}
---
+++ Hack/Hack/model/Supplier.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hack.model
+{
+    internal class Supplier:User
+    {
+        public int Id { get; set; }
+        public virtual ICollection<Order> Orders { get; set; } = new List<Order>();
+    }
+}
