c8551a63decdd7157bbcedcc94aa00f04681e86f
adoronin158@gmail.com <adoronin158@gmail.com>
2022-04-19 10:36:12 +0300

    228

---
+++ Hack/Hack/Personal Area.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Hack
+{
+    /// <summary>
+    /// Логика взаимодействия для Personal_Area.xaml
+    /// </summary>
+    public partial class Personal_Area : Window
+    {
+        public Personal_Area()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Hack/Hack/Purchaser.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Hack
+{
+    /// <summary>
+    /// Логика взаимодействия для Purchaser.xaml
+    /// </summary>
+    public partial class Purchaser : Window
+    {
+        public Purchaser()
+        {
+            InitializeComponent();
+        }
+    }
+}
